2a71bf3ed7a3b0a3a5aa4f5c8a910ebe4d97dc32
fernandogentilp <gentilcx1@gmail.com>
2023-03-29 15:48:21 -0300

    Mais um exercício sobre operadores booleanos

---
+++ tipo_curso.py
@@ -0,0 +1,9 @@
+curso = input()
+
+if curso == 'Matemática' or curso == 'Ciência da Computação' or curso == 'Engenharias':
+    print('exatas')
+if curso == 'Biologia' or curso == 'Física' or curso == 'Química':
+    print('Ciẽncias naturais')
+if curso == 'Geografia' or curso == 'Línguas' or curso == 'Ciências Sociais':
+    print('humanas')
+
